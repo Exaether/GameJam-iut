5414cd2077dd0cc4d4d498e43ca9c9888cf12f4c
ducruet.leo1 <ducruet.leo1@gmail.com>
2025-09-04 10:11:59 +0200

    Refactoring enemy class with constant values

--- entities/enemy.py
+++ entities/enemy.py
@@ -3,29 +3,34 @@ import math
 from core.settings import Settings 
 
 class Enemy(pygame.sprite.Sprite):
+
+    SPRITE_SIZE = 16
+    GUARD_SPEED = 0.1
+    COLLISION_AREA_WIDTH = 16
+    COLLISION_AREA_HEIGHT = 16
     
     def __init__(self, base_x=0, base_y=0, target_x=0, target_y=0):
         super().__init__()
         self.sprite_sheet = pygame.image.load('assets/entities/enemy_sprite.png')
-        self.image = pygame.Surface([16, 16])
+        self.image = pygame.Surface([self.SPRITE_SIZE, self.SPRITE_SIZE])
         self.get_image(0, 0)
         self.rect = self.image.get_rect()
         self.direction = "right"
-        self.path = "go" # "go" à a l'aller vers les coordonnées target et "back" au retour
+        self.path = "go" # "go" à l'aller vers les coordonnées target et "back" au retour
         self.x = base_x
         self.y = base_y
         self.base_x = base_x
         self.base_y = base_y
         self.target_x = target_x
         self.target_y = target_y
-        self.detection_area = pygame.rect.Rect(self.rect.left + 16, self.rect.top, 16, 16)
+        self.detection_area = pygame.rect.Rect(self.rect.left + self.SPRITE_SIZE, self.rect.top, self.COLLISION_AREA_WIDTH, self.COLLISION_AREA_HEIGHT)
 
     """
     Récupère l'image souhaitée sur la feuille de sprite
     """
     def get_image(self, x=0, y=0):
-        self.image = pygame.Surface([16, 16])
-        self.image.blit(self.sprite_sheet, (0, 0), (x, y, 16, 16))
+        self.image = pygame.Surface([self.SPRITE_SIZE, self.SPRITE_SIZE])
+        self.image.blit(self.sprite_sheet, (0, 0), (x, y, self.SPRITE_SIZE, self.SPRITE_SIZE))
         self.image.set_colorkey([0, 0, 0])
 
     """
@@ -50,43 +55,42 @@ class Enemy(pygame.sprite.Sprite):
         return self.detection_area.colliderect(rect)
 
     def update(self):
-        settings = Settings() 
         # Va vers les coordonnées target_x puis target_y
         if self.path == "go":
-            if not math.isclose(self.x, self.target_x, abs_tol=settings.GUARD_SPEED):
+            if not math.isclose(self.x, self.target_x, abs_tol=self.GUARD_SPEED):
                 if self.x < self.target_x:
-                    self.x += settings.GUARD_SPEED
+                    self.x += self.GUARD_SPEED
                     self.set_direction("right")
                 else:
-                    self.x -= settings.GUARD_SPEED
+                    self.x -= self.GUARD_SPEED
                     self.set_direction("left")
-            elif not math.isclose(self.y, self.target_y, abs_tol=settings.GUARD_SPEED):
+            elif not math.isclose(self.y, self.target_y, abs_tol=self.GUARD_SPEED):
                 if self.y < self.target_y:
-                    self.y += settings.GUARD_SPEED
+                    self.y += self.GUARD_SPEED
                 else:
-                    self.y -= settings.GUARD_SPEED
+                    self.y -= self.GUARD_SPEED
             else:
                 self.path = "back"
 
         # Reviens vers les coordonnées base_y puis base_x (refait donc le même chemin mais dans l'autre sens)
         elif self.path == "back":
-            if not math.isclose(self.y, self.base_y, abs_tol=settings.GUARD_SPEED):
+            if not math.isclose(self.y, self.base_y, abs_tol=self.GUARD_SPEED):
                 if self.y < self.base_y:
-                    self.y += settings.GUARD_SPEED
+                    self.y += self.GUARD_SPEED
                 else:
-                    self.y -= settings.GUARD_SPEED
-            elif not math.isclose(self.x, self.base_x, abs_tol=settings.GUARD_SPEED):
+                    self.y -= self.GUARD_SPEED
+            elif not math.isclose(self.x, self.base_x, abs_tol=self.GUARD_SPEED):
                 if self.x < self.base_x:
-                    self.x += settings.GUARD_SPEED
+                    self.x += self.GUARD_SPEED
                     self.set_direction("right")
                 else:
-                    self.x -= settings.GUARD_SPEED
+                    self.x -= self.GUARD_SPEED
                     self.set_direction("left")
             else:
                 self.path = "go"
 
         self.rect.topleft = (self.x, self.y)
         if self.direction == "right":
-            self.detection_area = pygame.rect.Rect(self.rect.left + 16, self.rect.top, 16, 16)
+            self.detection_area = pygame.rect.Rect(self.rect.left + self.SPRITE_SIZE, self.rect.top, self.COLLISION_AREA_WIDTH, self.COLLISION_AREA_HEIGHT)
         elif self.direction == "left":
-            self.detection_area = pygame.rect.Rect(self.rect.left - 16, self.rect.top, 16, 16)
+            self.detection_area = pygame.rect.Rect(self.rect.left - self.SPRITE_SIZE, self.rect.top, self.COLLISION_AREA_WIDTH, self.COLLISION_AREA_HEIGHT)
