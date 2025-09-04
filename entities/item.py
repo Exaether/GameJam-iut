4ccc4fbda0394ba4e3a5a4753fd9585f74bbedb5
Lukas Faure <lukas.faure@etu.univ-grenoble-alpes.fr>
2025-09-04 10:13:13 +0200

    refactor(item) clear code inutile

--- entities/item.py
+++ entities/item.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.dirname(__file__)
 "Item objectif du joueur"
 class Item(pygame.sprite.Sprite):
     def __init__(self,x,y):
-        super().__init__() # initialisation d'un sprite de la class pygame.sprite.Sprite
+        super().__init__()
         img_number = random.randint(1, 12)
-        img_path = os.path.join("assets", "items", f"{img_number}.png") # Chemin complet vers l'image
+        img_path = os.path.join("assets", "items", f"{img_number}.png")
 
         self.image = pygame.image.load(img_path).convert_alpha()
         self.pickable = True
@@ -19,15 +19,3 @@ class Item(pygame.sprite.Sprite):
         self.item_height = self.image.get_height()
         self.rect = self.image.get_rect()
         self.rect.center = x, y
-
-    def player_on_item(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom) and self.pickable == True:
-            self.pickable = False
-            return True # il faut supprimer dans le jeu le Item pour qu'il ne soit plus affiché
-        return False
-    
-    def get_item_width(self):
-        return self.item_width
-    
-    def get_item_height(self):
-        return self.item_height
